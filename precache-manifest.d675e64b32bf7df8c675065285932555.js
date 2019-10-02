4251d87e56e1a2c3d28485ce942790b0efcd8431
Nishkal <nishkal8463@gmail.com>
2019-10-02 23:33:49 +0530

    Updates

---
+++ precache-manifest.d675e64b32bf7df8c675065285932555.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54aac31ef3432c0f5abf337821148a93",
+    "url": "/Robo-search/index.html"
+  },
+  {
+    "revision": "61e3bc3a0af926de1004",
+    "url": "/Robo-search/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "48f26c9f4202d840069d",
+    "url": "/Robo-search/static/css/main.1db9b0fe.chunk.css"
+  },
+  {
+    "revision": "61e3bc3a0af926de1004",
+    "url": "/Robo-search/static/js/2.ccf12f60.chunk.js"
+  },
+  {
+    "revision": "48f26c9f4202d840069d",
+    "url": "/Robo-search/static/js/main.876c7167.chunk.js"
+  },
+  {
+    "revision": "7c4db9157dfcd5ae70a3",
+    "url": "/Robo-search/static/js/runtime-main.8fca4567.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/Robo-search/static/media/SEGA.82e09f78.TTF"
+  }
+]);
